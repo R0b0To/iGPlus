d7d5d2ddbcd1ce9089058b474c4f355e8b0af9b0
R0b0To <atonrhai@gmail.com>
2022-09-24 15:15:57 +0200

    fix

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,27 +1,48 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    var deleteButton = document.getElementById('delete');
-    var select_data = document.getElementById('select');
-    var newButton = document.getElementById('new');
-    var recapButton = document.getElementById('recap');
-    var averageButton = document.getElementById('average');
-    var pitButton = document.getElementById('pit'); 
+     deleteButton = document.getElementById('delete');
+     select_data = document.getElementById('select');
+     newButton = document.getElementById('new');
+     recapButton = document.getElementById('recap');
+     averageButton = document.getElementById('average');
+     pitButton = document.getElementById('pit'); 
+    
+     //start_overtakes = document.getElementById('start');
+    is_save_empty = true;
+
+    function toggleButtons()
+    {
+        
+     if(is_save_empty){
+        recapButton.disabled = true;
+        averageButton.disabled = true;
+        pitButton.disabled = true;
+      }
+      else{
+        recapButton.disabled = false;
+        averageButton.disabled = false;
+        pitButton.disabled = false;
+      }
+    }
+
+
 
 //get names of save data
     chrome.storage.local.get(null, function(data) {
 
         if(typeof data.current_managers === 'undefined')
-        {    
-          console.log("data not found");  
+        {            
+          console.log("data not found");
         }else{
           storage_list = Object.keys(data);
           valid_saves = storage_list.filter(name => name.includes('LRID'));
-          //console.log(storage_list);
-          if(valid_saves.length==0){
+
+          //generate a "default" save for the data that was found but not named
+          if(valid_saves.length==0)
+          {
 
             chrome.storage.local.get('current_managers', function(season_data) {
-              //console.log(season_data.current_managers);
               if(typeof season_data.current_managers === 'undefined')
               {    
                   console.log("no data");
@@ -30,18 +51,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 option.textContent = "default";
                 current_data = "defaultLRID";
                 chrome.storage.local.set({'current_data':current_data}, function() {
-
                 }); 
                 select_data.appendChild(option);              
               }
 
 
             });
-
-
          }
 
-        // alert(data.current_data);
+         //generate selection menu with stored data
           for(i=0; i < valid_saves.length; i++)
           {
             option = document.createElement("option"); 
@@ -55,27 +73,61 @@ document.addEventListener('DOMContentLoaded', function() {
           
           chrome.storage.local.set({[data.current_data]:data.current_managers}, function() {
             console.log(data);
+            if(data.current_managers==0)
+            is_save_empty = true;
+            else{
+              is_save_empty = false;
+            }
+            
+            
+            toggleButtons();
           }); 
-          
-
-
-        }
+    
+        }     
      
     });
 
 
+    
+  /*  start_overtakes.addEventListener('click',function(){
+      chrome.storage.local.get('current_managers', function(data) {
+        if(is_data_loaded)
+        {
+            
+                
+        }else{
+        
+        }
+
+
+
+      });
+
+
+    });*/
+    
+    
+    
     select_data.addEventListener('change', select_change);
  
     //SELECT CHANGE
     function select_change(){
  
       chrome.storage.local.get(null, function(data,) {
-        current_data = select_data.options[select_data.selectedIndex].text+"LRID";
-        chrome.storage.local.set({[data.current_data]:data.current_managers}, function() {
-          chrome.storage.local.set({'current_data':current_data});
+        selection_opt = select_data.options[select_data.selectedIndex].text+"LRID";
+        chrome.storage.local.set({[data.selection_opt]:data.current_managers}, function() {
+          chrome.storage.local.set({'current_data':selection_opt});
           console.log("saving current managers in "+data.current_data);               
-          chrome.storage.local.get(current_data, function(data) {
-            chrome.storage.local.set({'current_managers':data[current_data]}, function() {});
+          chrome.storage.local.get(selection_opt, function(data) {
+            chrome.storage.local.set({'current_managers':data[selection_opt]}, function() {
+              
+              if(data[selection_opt]==0)
+                is_save_empty = true;
+                else
+                is_save_empty = false;
+
+                toggleButtons();
+            });
           });
         
         });
@@ -84,21 +136,15 @@ document.addEventListener('DOMContentLoaded', function() {
       
 
 
-
     }
-
- 
 
     //RECAP BUTTON
     recapButton.addEventListener('click', function(){
-      chrome.storage.local.get('current_managers', function(season_data) {
 
-        if(typeof season_data.current_managers === 'undefined' || season_data.current_managers === 0)
-        {
-            console.log("empty");     
-        }else{
+     
+        chrome.storage.local.get('current_managers', function(season_data) {
 
-          //sorting managers by quali
+               //sorting managers by quali
           sortQuali = season_data.current_managers.sort((a,b) =>{
             if(a.quali > b.quali)
             return 1
@@ -138,27 +184,22 @@ document.addEventListener('DOMContentLoaded', function() {
                     }
           
           //reporttext.textContent=race_timings;       
-          downloadFile(race_timings,"race_recap");
-        }    
+          downloadFile(race_timings,"race_recap"); 
+          
 
-    })});
-
+    });
+        
+    });
 
     //PIT BUTTON
     pitButton.addEventListener('click',function(){
-      chrome.storage.local.get('current_managers', function(season_data) {
 
-        if(typeof season_data.current_managers === 'undefined' || season_data.current_managers === 0)
-        {
-            console.log("empty data"); 
-                
-        }else{
-          
+      
+          chrome.storage.local.get('current_managers', function(season_data) {
           season_data.current_managers.sort((a, b) => {
             return a.race_finish - b.race_finish;
         });
           string_format ="";
-          
           for(i=0 ; i< season_data.current_managers.length; i++)
           {
             string_format+=(i+1);
@@ -166,77 +207,46 @@ document.addEventListener('DOMContentLoaded', function() {
             string_format+=season_data.current_managers[i].name;
             string_format+=", ";
             string_format+=season_data.current_managers[i].pit_stop.toString();
-           // string_format+=",";
-            //string_format+=season_data.current_managers[i].race_replay.length;
             string_format+="\n";
             
           }
-          
-          console.log(string_format);
-          
           downloadFile(string_format,"pit_history");
-
-        }    
-
-    })});
+    });
+      
+    });
 
      //Driver Average
-     averageButton.addEventListener('click',function(){
-      chrome.storage.local.get('current_managers', function(data) {
+    averageButton.addEventListener('click',function(){
 
-        manager = data.current_managers;
-        if(typeof manager === 'undefined' || manager === 0)
-        {
-            console.log("empty data"); 
-                
-        }else{
+        chrome.storage.local.get('current_managers', function(data) {
+          manager = data.current_managers;
+            string = "Driver/Rank,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32\n";
+  
+             manager.forEach(driver => {
+               laps_position = Array(32).fill(0);
+              for(i=0 ; i <= driver.rank.length ; i++)
+              {
+                 laps_position[driver.rank[i]-1] += 1;
+              }
+  
+              string+=driver.name+","+laps_position.toString()+"\n";
+              //console.log(driver.name+" "+laps_position);
+  
+            });
+  
+            console.log(string);
+           downloadFile(string,"lap_trend");  
+      });
+      
+    });
 
-          string = "Driver/Rank,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32\n";
-
-           manager.forEach(driver => {
-             laps_position = Array(32).fill(0);
-            for(i=0 ; i <= driver.rank.length ; i++)
-            {
-               laps_position[driver.rank[i]-1] += 1;
-            }
-
-            string+=driver.name+","+laps_position.toString()+"\n";
-            //console.log(driver.name+" "+laps_position);
-
-          });
-
-          console.log(string);
-         downloadFile(string,"lap_trend");
-
-        }    
-
-    })});
-
-    //DOWNLOAD DATA
-function downloadFile(data,download_name){
-  var blob = new Blob([data], { type: 'text/csv;charset=utf-8;' });
-  if (navigator.msSaveBlob) { // IE 10+
-      navigator.msSaveBlob(blob, "test");
-  } else {
-      var link = document.createElement("a");
-      if (link.download !== undefined) { // feature detection
-          // Browsers that support HTML5 download attribute
-          var url = URL.createObjectURL(blob);
-          link.setAttribute("href", url);
-          link.setAttribute("download", download_name);
-          link.style.visibility = 'hidden';
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-      }
-  }
-}
     //NEW RACE BUTTON
     newButton.addEventListener('click', function(){
       
       leagueName = prompt("enter league name");
-      leagueNameId =leagueName+"LRID";
+      leagueNameId =leagueName+"LRID"; //LRID is to avoid naming conflicts
       
+      //save before creating new data
       chrome.storage.local.get(null, function(data) { 
         if(!typeof data.current_data === 'undefined')
         {
@@ -256,25 +266,20 @@ function downloadFile(data,download_name){
           select_data.appendChild(option);
        
           chrome.storage.local.set({[leagueNameId]:0, 'current_managers':0, 'current_data':current_data}, function() {
-
+            is_save_empty = true;
+            toggleButtons();
           });                   
-          
-
-        
+                 
       });
-      
-
 
     });
-
 
     //DELETE BUTTON
     deleteButton.addEventListener('click', function(){
 
       try {
         opt = select_data.options[select_data.selectedIndex].text+"LRID";
-        
-      } catch (error) {
+      } catch (error) { 
         alert("nothing to delete");
         return;
       }
@@ -283,13 +288,27 @@ function downloadFile(data,download_name){
       chrome.storage.local.remove(opt, function() {
         select_data.remove(select_data.selectedIndex); 
         try {
+          //after removing try to pick the next element from the html selection
           opt = select_data.options[select_data.selectedIndex].text+"LRID";
         } catch (error) {
+          //it means the selection list is empty
           chrome.storage.local.remove("current_managers");
           return;
         }
           chrome.storage.local.get(opt, function(data) {
-          chrome.storage.local.set({'current_data':opt,'current_managers':data[opt]});
+
+            console.log(data[opt]);
+            if(data[opt]==0 || data[opt]=="undefined")         
+              is_save_empty = true;
+            else
+              is_save_empty = false;
+            
+            
+
+          chrome.storage.local.set({'current_data':opt,'current_managers':data[opt]},function(){
+            toggleButtons();
+          });
+
         });
         
         
@@ -301,5 +320,25 @@ function downloadFile(data,download_name){
 
   });
 
+
+      //DOWNLOAD DATA
+function downloadFile(data,download_name){
+  var blob = new Blob([data], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, "test");
+  } else {
+      var link = document.createElement("a");
+      if (link.download !== undefined) { // feature detection
+          // Browsers that support HTML5 download attribute
+          var url = URL.createObjectURL(blob);
+          link.setAttribute("href", url);
+          link.setAttribute("download", download_name);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      }
+  }
+}
   
   
