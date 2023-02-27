15741fd9d1ec988e2ff9816ed371ff7ae65457f7
evhenious <evhenious@gmail.com>
2023-02-27 20:11:44 +0200

    almost done with Research page

--- Extension/research.js
+++ Extension/research.js
@@ -1,15 +1,20 @@
-async function addTable() {
+async function enhanceResearchTable() {
   const { language } = await chrome.storage.local.get({ language: 'eng' });
-  const { language: i18n } = await import('./common/localization.js');
+  const { language: i18n } = await import(chrome.runtime.getURL('/common/localization.js'));
+
+  const observer = new MutationObserver(function (mutations) {
+    weightedResearch();
+    calculateTotalResearchGain();
+  });
 
   const researchPowerSpan = document.getElementById('checkboxTotal');
   const currentResearchPower = researchPowerSpan.textContent.slice(0, -1) / 100;
 
   if (document.getElementById('statsTable') == null) {
-    const table = document.createElement('table');
-    table.id = 'statsTable';
-    table.classList.add('acp', 'hoverCopy');
-    table.style.width = 'auto'; // important to have it here, otherwise game's .acp overwrites it always
+    const statsTable = document.createElement('table');
+    statsTable.id = 'statsTable';
+    statsTable.classList.add('acp', 'hoverCopy');
+    statsTable.style.width = 'auto'; // important to have it here, otherwise game's .acp overwrites it always
 
     const header = document.createElement('thead');
     header.append(
@@ -18,86 +23,82 @@ async function addTable() {
       createHeaderWithImage('images/gap.svg'),
       createHeaderWithImage('images/research.svg')
     );
-    table.append(header);
+    statsTable.append(header);
 
     const gameTable = document.getElementById('carResearch');
 
-    const body = document.createElement('tbody');
-
     /** @type { NodeListOf<HTMLDivElement> } */
-    const ratings = gameTable.querySelectorAll('.ratingBar');
-    ratings.forEach((bar) => {
+    const ratingBars = gameTable.querySelectorAll('.ratingBar');
+    const researchStatsRows = [...ratingBars].map((bar) => {
       const row = document.createElement('tr');
       row.className = 'hoverCopyTr';
 
       const scaleFactor = 2; //? is it always 2? I guess it depends on current tier
       const bestTeamValue = /(\d+)/.exec(bar.querySelector('img').style.left)[0] * scaleFactor;
-      const myValue =parseInt(bar.querySelector('div').style.width) * scaleFactor;
+      const myValue = parseInt(bar.querySelector('div').style.width) * scaleFactor;
 
       const ratingGap = bestTeamValue - myValue;
       const isChecked = bar.closest('tr').querySelector('input').checked;
+      row.dataset.id = bar.closest('tr').querySelector('input').value;
 
       let gain = 0;
       if (isChecked) {
         gain = Math.ceil(ratingGap * currentResearchPower);
       }
 
-      row.append(
-        createTd(myValue),
-        createTd(bestTeamValue),
-        createTd(ratingGap),
-        createTd(gain)
-      );
+      row.append(createTd(myValue), createTd(bestTeamValue), createTd(ratingGap), createTd(gain));
 
-      body.append(row);
+      return row;
     });
 
-    table.append(body);
+    const body = document.createElement('tbody');
+    body.append(...researchStatsRows);
+    statsTable.append(body);
 
-    try {
-      const realAttr = sortArray(document.getElementById('overview').querySelectorAll('[class*=block]'));
-      //body.rows[0].childNodes[0].textContent;
-      for (let i = 0; i < 8; i += 1) {
-        const sponsorValue = realAttr[i].textContent - body.rows[i].childNodes[0].textContent;
-        body.rows[i].childNodes[0].append(realCarDiff(sponsorValue));
+    // TODO improve this. Looks like sponsor values are inaccurate if get the diff from rating bars
+    const currentCarAttributes = [...document.querySelectorAll('#overview #carAttribTable [class*=block]')].map((node) => ({
+      id: node.parentElement.id,
+      value: node.textContent
+    }));
+
+    researchStatsRows.forEach((row) => {
+      const rowId = row.dataset.id;
+      const currentValue = currentCarAttributes.find(({ id }) => id.includes(rowId)).value;
+      const sponsorEffect = currentValue - row.childNodes[0].textContent;
+
+      if (sponsorEffect) {
+        row.childNodes[0].append(realCarDiff(sponsorEffect));
       }
-    } catch (error) {
-      console.log(error);
-    }
-    gameTable.parentElement.insertBefore(table, gameTable);
+    });
 
-    const helpmark = askHelpButton(i18n[language].researchHelp);
+    gameTable.parentElement.insertBefore(statsTable, gameTable);
+
+    const helpmark = createHelpButton(i18n[language].researchHelp);
 
     const tfoot = document.createElement('tfoot');
-    tfoot.setAttribute('style', 'background:#e3e4e5;z-index: 2;position: relative;');
+    tfoot.classList.add('researchFooter');
 
     const thf = document.createElement('th');
     thf.colSpan = 2;
-    thf.setAttribute('style', 'background: #e3e4e5;border-right:0');
-
-    tfoot.append(helpmark);
-    tfoot.append(thf);
 
     const total = document.createElement('td');
-    total.setAttribute('style', 'font-weight: bold;text-align: center;');
     total.id = 'totalGain';
 
-    tfoot.append(total);
-    table.append(tfoot);
+    tfoot.append(helpmark, thf, total);
+    statsTable.append(tfoot);
   }
 
   observer.observe(researchPowerSpan, { characterData: false, attributes: false, childList: true, subtree: false });
   weightedResearch();
-
-  pointGain();
+  calculateTotalResearchGain();
 }
 
 // TODO move to separate retry module?
 (async () => {
   for (let i = 0; i < 3; i += 1) {
     try {
-      await new Promise((res) => setTimeout(res, 200)); // sleep a bit, while page loads
-      addTable();
+      await new Promise((res) => setTimeout(res, 100)); // sleep a bit, while page loads
+      await enhanceResearchTable();
       break;
     } catch (err) {
       console.warn(`Retry to enhance research table #${i + 1}/3`);
@@ -110,7 +111,7 @@ function createTd(value) {
   ele.className = 'hoverCopyTd';
   ele.textContent = value;
 
-  ele.addEventListener('click', copyColumn);
+  ele.addEventListener('click', copyColumnData);
   ele.setAttribute('style', 'height:32px;text-align: center;');
 
   return ele;
@@ -129,26 +130,20 @@ function createHeaderWithImage(imgUrl) {
   return header;
 }
 
-function sortArray(a) {
-  return [a[0], a[2], a[4], a[6], a[1], a[3], a[5], a[7]];
-}
-
+/**
+ * @param {number} val
+ * @returns {HTMLSpanElement}
+ */
 function realCarDiff(val) {
-  if (val != 0) {
-    sponsorSpan = document.createElement('span');
-    sponsorSpan.setAttribute('style', 'font-size: x-small; position:absolute;');
-    if (val > 0) {
-      sponsorSpan.style.color = 'green';
-      sponsorSpan.textContent = ' +' + val;
-    } else {
-      sponsorSpan.style.color = 'red';
-      sponsorSpan.textContent = val;
-    }
-    return sponsorSpan;
-  } else return '';
+  const sponsorSpan = document.createElement('span');
+  sponsorSpan.classList.add('sponsorEffect', val > 0 ? 'positiveEffect' : 'negativeEffect');
+
+  sponsorSpan.textContent = val;
+
+  return sponsorSpan;
 }
 
-async function copyColumn() {
+async function copyColumnData() {
   const columnIndex = this.cellIndex;
   const table = this.closest('tbody');
 
@@ -161,6 +156,7 @@ async function copyColumn() {
   console.log('text copied');
 }
 
+// TODO revisit and finish here
 function weightedResearch() {
   try {
     rPower = parseFloat(document.getElementById('checkboxTotal').textContent.slice(0, -1) / 100);
@@ -188,7 +184,7 @@ function weightedResearch() {
           fe: 0.1,
           te: 0.1,
           col: 0.01,
-          rel: 0.01,
+          rel: 0.01
         };
         return (2.23 + 4.23 * Math.log(currentS + gap * rPower) - (2.23 + 4.23 * Math.log(currentS))) * weight[code];
       }
@@ -209,78 +205,69 @@ function weightedResearch() {
   } catch (error) {}
 }
 
-var observer = new MutationObserver(function (mutations) {
-  weightedResearch();
-  pointGain();
-});
+function createHelpButton(text) {
+  const fieldTip = document.createElement('span');
+  fieldTip.id = 'fieldtip';
 
-function addFieldTip() {
-  var span = document.createElement('span');
-  span.id = 'fieldtip';
-  span.setAttribute('style', `opacity:0`);
-  return span;
-}
+  const helpLabel = document.createElement('span');
+  helpLabel.textContent = '?';
+  helpLabel.classList.add('helpLabel');
 
-function askHelpButton(text) {
-  span = document.createElement('span');
-  container = document.createElement('td');
-  span.textContent = '?';
-  span.className = 'fieldtip';
-  span.setAttribute(
-    'style',
-    'cursor: help;display:block; text-align: center; ; border-radius: 50%;background-color: #96bf86;color: #ffffff;width: 23px;height: 23px;'
-  );
-  span.setAttribute('data-fieldtip', text);
-  span.addEventListener('mouseenter', function () {
-    fieldtip = document.getElementById('fieldtip');
-    fieldtip.textContent = this.dataset.fieldtip;
-    fieldtip.style.display = 'inline-block';
-    var position = {
-      top: 355,
-      left: this.offsetLeft - 10,
-    };
-    fieldtip.style.opacity = 1;
+  helpLabel.setAttribute('data-helptext', text);
 
-    fieldtip.setAttribute('style', `top:${position.top}px;left:${position.left}px`);
+  // show help text
+  helpLabel.addEventListener('mouseenter', function () {
+    fieldTip.textContent = this.dataset.helptext;
+    fieldTip.classList.add('tipVisible');
   });
-  span.addEventListener('mouseleave', function () {
-    fieldtip = document.getElementById('fieldtip');
-    fieldtip.style.opacity = 0;
-    fieldtip.style.display = 'none';
+
+  // hide help text
+  helpLabel.addEventListener('mouseleave', function () {
+    fieldTip.classList.remove('tipVisible');
   });
-  //span =
+
   if (document.getElementById('fieldtip') == null) {
-    place = document.getElementsByClassName('bgGrey')[0];
-    place.append(addFieldTip());
+    const place = document.getElementsByClassName('bgGrey')[0];
+    place.append(fieldTip);
   }
-  container.append(span);
+
+  const container = document.createElement('td');
+  container.append(helpLabel);
+
   return container;
 }
 
-function pointGain() {
-  rPower = parseFloat(document.getElementById('checkboxTotal').textContent.slice(0, -1) / 100);
-  checkboxdAttr = document.querySelectorAll('input[type="checkbox"]:not(.checkAll)');
-  gametable = document.getElementById('statsTable').tBodies[0];
-  var totalGain = 0;
-  Object.keys(checkboxdAttr).forEach((area) => {
-    var gain = 0;
-    var areaIndex = checkboxdAttr[area].parentElement.parentElement.rowIndex - 1;
-    if (checkboxdAttr[area].checked) {
-      if (checkboxdAttr[area].parentElement.previousElementSibling.parentElement.classList.contains('bgLightGreen')) {
-        gain = Math.ceil(rPower * gametable.rows[areaIndex].cells[2].textContent * 1.1);
-      } else if (
-        checkboxdAttr[area].parentElement.previousElementSibling.parentElement.classList.contains('bgLightRed')
-      ) {
-        gain = Math.ceil(rPower * gametable.rows[areaIndex].cells[2].textContent * 0.5);
-      } else {
-        gain = Math.ceil(rPower * gametable.rows[areaIndex].cells[2].textContent);
+function calculateTotalResearchGain() {
+  const rPower = document.getElementById('checkboxTotal').textContent.slice(0, -1) / 100;
+  const checkedItems = document.querySelectorAll('input[type="checkbox"]:not(.checkAll)');
+  const gameTable = document.getElementById('statsTable').tBodies[0];
 
-        // console.log('neutraal');
+  let totalGain = 0;
+
+  Object.keys(checkedItems).forEach((area) => {
+    const areaIndex = checkedItems[area].parentElement.parentElement.rowIndex - 1;
+
+    if (!checkedItems[area].checked) {
+      gameTable.rows[areaIndex].cells[3].textContent = '';
+    }
+
+    let gain = '';
+    let scaleFactor = 1;
+    const researchAreaRow = checkedItems[area].parentElement.previousElementSibling.parentElement;
+    if (checkedItems[area].checked) {
+      if (researchAreaRow.classList.contains('bgLightGreen')) {
+        scaleFactor = 1.1;
       }
+      if (researchAreaRow.classList.contains('bgLightRed')) {
+        scaleFactor = 0.5;
+      }
+
+      gain = Math.ceil(rPower * gameTable.rows[areaIndex].cells[2].textContent * scaleFactor);
       totalGain += gain;
-      gametable.rows[areaIndex].cells[3].textContent = gain;
-    } else gametable.rows[areaIndex].cells[3].textContent = '';
+    }
+
+    gameTable.rows[areaIndex].cells[3].textContent = gain;
   });
+
   document.getElementById('totalGain').textContent = totalGain;
-  //console.log(totalGain);
 }
