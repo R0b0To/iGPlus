9990b2854380e60c689e9d24b055f67e92772e18
R0b0To <113817188+R0b0To@users.noreply.github.com>
2023-03-15 13:48:22 +0100

    added parsing of more driver vars

--- Extension/driver/driverHelpers.js
+++ Extension/driver/driverHelpers.js
@@ -2,35 +2,64 @@
    *
    * @param {Object} personData
    * @param {Object} personData.vars
-   * @param {Object} personData.vars.sSpecial
-   * @returns {{ skill: string|null, grade: string|null }}
+   *    * @returns {{ dName: string, favTrack: string, sTalent: string, sHeight: string, starRating: number, sBMI: float, tName: string, tLink: string|null, sSpecial: {skill:string, gradeId:string} }}
    */
-function parseSpecialSkill(personData) {
+function parseAttributes(personData) {
+  
+  function createHTMLElement(varName){
   const fragmentToParse = document.createElement('table');
-  fragmentToParse.innerHTML = personData.vars.sSpecial;
+  fragmentToParse.innerHTML = personData.vars[varName];
+  return fragmentToParse;
+  }
 
-  const skill = fragmentToParse.querySelector('span')?.textContent || null;
-  //const gradeText = fragmentToParse.querySelector('span')?.getAttribute('data-tip') || null;
-  const gradeId = fragmentToParse.querySelector('span').classList[0] || null;
+  const dName = createHTMLElement('dName').textContent.slice(1); //removing extra space
+  const favTrack = createHTMLElement('favTrack').textContent.slice(1);
+  const sTalent = createHTMLElement('sTalent').textContent;
+  const sHeight = toCentimeters(personData.vars.sHeight);
+  const starRating = /\d+/.exec(personData.vars.starRating);
+  const sBMI = createHTMLElement('sBMI').textContent;
+  const tName = createHTMLElement('tName').textContent;
+  const tLink = createHTMLElement('tName').querySelector('a')?.href  || null;
+  const skill = createHTMLElement('sSpecial').querySelector('span')?.textContent || null;
+  const gradeId = createHTMLElement('sSpecial').querySelector('span')?.classList[0] || null;
+  const sSpecial = {name:skill,grade:gradeId};
 
-  return {skill, gradeId};
+  return {dName, favTrack, sTalent, sHeight, starRating, sBMI, tName, tLink, sSpecial};
+}
+
+function toCentimeters(height) {
+  // Set up an object containing the conversion factors for feet and inches to centimeters
+  const units = {ft: 30.48, in: 2.54, cm: 100 };
+  // Check if the height value is in feet and inches or in centimeters
+  let valueInCentimeters;
+  if (height[1] == '\'') // If the height is in feet and inches, split the value into feet and inches
+  {
+    const [feet, inches] = height.split(' ');
+    // Convert the feet and inches to centimeters and add them together
+    valueInCentimeters = ((parseInt(feet) * units.ft)) + (parseInt(inches) * units.in);
+  }
+  else if (height[1] == '.')
+    valueInCentimeters = parseInt(height) * units.cm; // If the height is in meters
+  else
+    valueInCentimeters = parseInt(height) ; // If the height is in cm
+  return valueInCentimeters;
 }
 
 /**
  * @param {string} skill
- * @param {'strength'|'weakness'} type
+ * @param {'grade'} type
  * @returns {HTMLSpanElement}
  */
-function createSpecialSkillLabel({skill, gradeId}) {
+function createSpecialSkillLabel({name, grade}) {
   const skillSpan = document.createElement('span');
   skillSpan.style.position = 'absolute';
-  skillSpan.classList.add(gradeId);
-  if(skill != 'None')
-    skillSpan.textContent = skill;
+  skillSpan.classList.add(grade);
+  if(grade != 'specialA0')
+    skillSpan.textContent = name;
   return skillSpan;
 }
 
 export {
   createSpecialSkillLabel,
-  parseSpecialSkill
+  parseAttributes
 };
