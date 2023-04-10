8e4f30bdb6361168ac9e1ba501bffce51c137625
R0b0To <113817188+R0b0To@users.noreply.github.com>
2023-04-10 18:12:03 +0200

    reformated "new driver in academy alert"

--- Extension/timerAlert.js
+++ Extension/timerAlert.js
@@ -1,65 +1,67 @@
 function checkAcademyTimer()
 {
-    timerAlert = document.getElementById("academyAlert");
-    if(timerAlert==null)
-    {
-     url = `https://igpmanager.com/index.php?action=fetch&d=facility&id=11&csrfName=&csrfToken=`;
-     fetch(url)
-    .then(response => response.json())
-    .then(data => {
+  const timerAlert = document.getElementById('academyAlert');
+  if(timerAlert == null)
+  {
+    console.log('doing');
+    const url = 'https://igpmanager.com/index.php?action=fetch&d=facility&id=11&csrfName=&csrfToken=';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
         try {
-        r= /cdStyle=.+?>(.*?)<\/span/;
-        resetDate = r.exec(data.vars.options)[1];
-        injectNotification(resetDate);
+          const r = /cdStyle=.+?>(.*?)<\/span/;
+          const resetDate = r.exec(data.vars.options)[1];
+          injectNotification(resetDate);
         } catch (error) {
-            //academy lv 0
+          //console.log('academy not found');
+          injectNotification(new Date());
         }
-        
-    }) 
-    }else{
-      resetDate = document.getElementById("academyAlert").attributes.expire.textContent;
-      document.getElementById("academyAlert").childNodes[0].textContent = countDown(resetDate);
-    }
-   
+
+      });
+  }else{
+    const resetDate = document.getElementById('academyAlert').attributes.expire.textContent;
+    document.getElementById('academyAlert').childNodes[0].textContent = countDown(resetDate);
+  }
+
 }
 function injectNotification (resetDate){
-    if(document.getElementById("academyAlert")==null)
-    {
-    notification = document.createElement("div"); 
-    notification.className = "notify";
-    notification.id = "academyAlert";
-    notification.setAttribute("style","display:flex;background:#5986b3!important;width: fit-content!important;min-width: 24px;");
-    notification.setAttribute("expire",resetDate);
-    span = document.createElement("span");
-    span.className= "robotoBold";
+  if(document.getElementById('academyAlert') == null)
+  {
+    const notification = document.createElement('div');
+    notification.className = 'notify';
+    notification.id = 'academyAlert';
+    notification.setAttribute('style','display:flex;background:#5986b3!important;width: fit-content!important;min-width: 24px;');
+    notification.setAttribute('expire',resetDate);
+    const span = document.createElement('span');
+    span.className = 'robotoBold';
     span.textContent = countDown(resetDate);
     notification.append(span);
     try {
-    hq = document.getElementById("mHeadquarters");
-    hq.style.position = "relative";
-    hq.append(notification); 
+      const hq = document.getElementById('mHeadquarters');
+      hq.style.position = 'relative';
+      hq.append(notification);
     } catch (error) {//error
     }
-    }
+  }
 }
 function countDown(resetDate) {
-var countDownDate = new Date(resetDate).getTime();
-    var now = new Date().getTime();
-    var ms = countDownDate - now;
-    var d, h, m, s;
-    s = Math.floor(ms / 1000);
-    m = Math.floor(s / 60);
-    s = s % 60;
-    h = Math.floor(m / 60);
-    m = m % 60;
-    d = Math.floor(h / 24);
-    h = h % 24;
-    function is0(n,t){if(n==0)return "";return n+t}
-    string = `${is0(d,"d")}${is0(h,"h")}${is0(m,"m")}`;   
-    if (ms < 0)
-      string = "!";
+  var countDownDate = new Date(resetDate).getTime();
+  var now = new Date().getTime();
+  var ms = countDownDate - now;
+  var d, h, m, s;
+  s = Math.floor(ms / 1000);
+  m = Math.floor(s / 60);
+  s = s % 60;
+  h = Math.floor(m / 60);
+  m = m % 60;
+  d = Math.floor(h / 24);
+  h = h % 24;
+  function is0(n,t){if(n == 0)return '';return n + t;}
+  let string = `${is0(d,'d')}${is0(h,'h')}${is0(m,'m')}`;
+  if (ms < 0)
+    string = '!';
 
-    return string;
-    //return { d: d, h: h, m: m, s: s };
-  }
+  return string;
+  //return { d: d, h: h, m: m, s: s };
+}
 setTimeout(checkAcademyTimer, 500);
