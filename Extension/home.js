8d1950f0d19e5726d05abf1dec825c64c6233359
R0b0To <113817188+R0b0To@users.noreply.github.com>
2023-04-13 14:46:14 +0200

    added more shortuts to home page when racing

--- Extension/home.js
+++ Extension/home.js
@@ -8,17 +8,52 @@ function addExtraButtons() {
     previousRaceBtn.classList.remove('fill-w');
     previousRaceBtn.classList.add('home-prev-race'); // need this to not interfere with other .btn2
     previousRaceBtn.removeAttribute('style', ''); // let's drive with css
-
-    const raceReviewBtn = document.createElement('a');
-    raceReviewBtn.id = 'raceReview';
-    raceReviewBtn.href = 'd=raceReview';
-    raceReviewBtn.classList.add('btn4');
-    raceReviewBtn.textContent = 'Race Review';
-
-    previousRaceBtn.after(raceReviewBtn);
+    previousRaceBtn.after(createdButton('raceReview','d=raceReview','Race Review'));
+    
   } catch (err) {
     console.warn(err);
   }
 }
 
-addExtraButtons();
+addExtraButtons();whenLockedSetup();
+
+
+function whenLockedSetup(){
+  const raceID = document.getElementById('mRace').href.replace(/^\D+/g, '');
+  if(raceID != '')injectLockedShortcuts(raceID);
+  
+  
+  
+
+}
+function injectLockedShortcuts(raceID){
+  const setupURL = 'p=race&tab=setup';
+  const strategyURL = 'p=race&tab=strategy';
+  const qualiURL = `d=result&id=${raceID}&tab=qualifying`;
+  const shortcutsLocation = document.getElementById('splashPrep');
+  
+  const setupbtn = createdButton('lockedsetup',setupURL,'Setup');
+  const strategybtn = createdButton('lockedstrat',strategyURL,'Strategy');
+  const qualibtn = createdButton('lockedquali',qualiURL,'Qualifying');
+  const container = document.createElement('div');
+  [setupbtn,strategybtn,qualibtn].forEach(btn => {
+    btn.style.flexGrow = 1;
+    btn.style.margin = '2px';}
+    );
+  container.setAttribute('style','display: flex;justify-content: center;margin-top: 5px;');
+
+  container.append(setupbtn,strategybtn,qualibtn);
+
+  if(document.getElementById('lockedsetup')==null)
+  shortcutsLocation.append(container);
+
+}
+
+function createdButton(id,link,name){
+  const btn = document.createElement('a');
+  btn.id = id;
+  btn.classList.add('btn');
+  btn.href = link;
+  btn.textContent = name;
+  return btn;
+}
