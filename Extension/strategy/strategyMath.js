5f5b75c21ca5048d644ed58fb37f9e70f1da3e52
R0b0To <113817188+R0b0To@users.noreply.github.com>
2023-07-04 15:55:34 +0200

    tweaking wear when pushing

--- Extension/strategy/strategyMath.js
+++ Extension/strategy/strategyMath.js
@@ -20,22 +20,22 @@ function fuel_calc(f){
   }}
 
 function get_wear(tyre,laps,track_info,car_economy,multiplier){
-  console.log(tyre,laps,track_info,car_economy,multiplier);
+  //console.log(tyre,laps,track_info,car_economy,multiplier);
 
   //TO DO. if wear is needed for strategy preview make a request to open.weather with specified track
   const weatherNow = convertToCelsius((document.getElementsByClassName('pWeather text-right green')[0]?.lastChild.textContent ) ?? '25');
-  console.log('past temperature')
+  //console.log('past temperature')
   const optimal = -0.0323 * track_info.avg  + 10.9 - (weatherNow / 10);
   const tyreScale = {SS:optimal - 1,S:optimal,M:optimal + 1,H:optimal + 2,I:optimal + 1,W:optimal + 1};
 
-  console.log(tyre,tyreScale[tyre],5 - car_economy.push,'difference is:',tyreScale[tyre] - (5 - car_economy.push));
+  //console.log(tyre,tyreScale[tyre],5 - car_economy.push,'difference is:',tyreScale[tyre] - (5 - car_economy.push));
   const diff =   tyreScale[tyre] - (5 - car_economy.push);
   let pushDecay = 0;
   switch (true) {
   case diff >= 1 && diff <= 2:
     pushDecay = 0.3;   break;
   case diff >= 2 && diff <= 3:
-    pushDecay = 0.8;   break;
+    pushDecay = 0.5;   break;
   case diff >= 3:
     pushDecay = 1.5;   break;
   case diff >= 0  && diff <= 1:
@@ -50,47 +50,39 @@ function get_wear(tyre,laps,track_info,car_economy,multiplier){
     pushDecay = 0;
   }
 
-
   const tyreWearFactors = {SS: 2.14,S: 1.4,M: 1,H: 0.78};
-  //const tyreWearFactors = {SS: 2.25,S: 1.5,M: 1,H: 0.9};
-  const decayScale = {SS: 1,S: 0.9,M: 1.15,H: 1.15};
-  const tyreWear  = tyreWearFactors[tyre] ?? 1;
 
+  const tyreWear  = tyreWearFactors[tyre] ?? 1;
   const track_wear = track_info.wear;
   const track_length = track_info.length;
   const te = car_economy.te;
-  /* const track_wear = 65;
-  const track_length = 4.60296;
-  const te = 12;*/
 
   const t = (1.43 * te ** -0.0778) * (0.00364 * track_wear + 0.354) * track_length * 1.384612 * multiplier * tyreWear;
 
-  
   //calculate stint wear
-  const stint = Math.exp(1) ** ((-t / 100 * 1.18) * laps) * 100;
+  /*const stint = Math.exp(1) ** ((-t / 100 * 1.18) * laps) * 100;
   let stint2 = (1 - (1 * ((t) + (0.0212 * laps - 0.00926) * track_length) / 100));
   for(let j = 1 ; j < laps ; j++)
   {
     stint2 *= (1 - (1 * ((t) + (0.0212 * j - 0.00926) * track_length) / 100));
   }
-  let stint7 = 100 - (t / 100);
-  for (var i = 1; i <= laps; i++) {
-    stint7 = stint7 - (stint7 * t / 100);
-  }
-  stint2 = stint2 * 100;
-  const average = ((stint + stint2) / 2).toFixed(2);
-  const stint3 = calculateWear(laps,t,pushDecay);
+  stint2 = stint2 * 100;*/
+  //const average = ((stint + stint2) / 2).toFixed(2);
+  const stint3 = calculateWear(laps,t,pushDecay,diff);
 
-  console.log('laps:',laps,stint3);
+  //console.log('laps:',laps,stint3);
 
   // return average;
   return stint3;
 }
 
-function calculateWear(n, wear, push) {
-  console.log('doing',n,'laps','using push',push,wear);
-  console.log((1 - wear / 100 + push / 100))
+function calculateWear(n, wear, push,diff) {
   let lapWear = ((1 - wear / 100 + push / 100) ** n) * 100;
+  if(n > 3 && diff <= -2){
+    console.log('old push',lapWear)
+    lapWear = ((1 - wear/100 + push/100 -0.013)** n)*100;
+    console.log('new push',lapWear)
+  }
   /*if(lapWear<60)
         lapWear = ((1 - wear/100 + push/100 -0.002)** n)*100;
   console.log(lapWear)*/
