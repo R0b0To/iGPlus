6fa657617d63a4c5e2c968244183a03e6b56255f
R0b0To <atonrhai@gmail.com>
2023-12-27 23:31:11 +0100

    logic check

--- Extension/common/darkmode.js
+++ Extension/common/darkmode.js
@@ -3,23 +3,22 @@ var darkmode = document.createElement("div");
 darkmode.id = "igplus_darkmode";
 
 darkmode.innerHTML = '<style>' + rule + '</style>';
-
-if(!document.getElementById('igplus_darkmode'))
-{
    document.body.append(darkmode);   
 }
-
+if(!document.getElementById('igplus_darkmode'))
+{
+  const file = chrome.runtime.getURL('common/darkmode.css');
+  fetch(file)
+    .then(response => response.text())
+    .then(cssContent => {
+      // Do something with the CSS content
+      injectStyles(cssContent)
+    })
+    .catch(error => {
+      console.error('Error fetching file:', error);
+    });
 }
-const file = chrome.runtime.getURL('common/darkmode.css');
-fetch(file)
-  .then(response => response.text())
-  .then(cssContent => {
-    // Do something with the CSS content
-    injectStyles(cssContent)
-  })
-  .catch(error => {
-    console.error('Error fetching file:', error);
-  });
+
 
     
   
